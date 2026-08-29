25a0af2a9cddb31c2f3157819b9a265826724ee6
david cobbold <144285585+Dvcutie04@users.noreply.github.com>
2026-08-28 21:13:06 -0700

    Update mock_tv.py

--- src/device_fabric/mocks/mock_tv.py
+++ src/device_fabric/mocks/mock_tv.py
@@ -2,27 +2,37 @@ from typing import Optional, Tuple
 from src.device_fabric.contracts import DeviceState
 
 
-class MockTVAdapter:
-    def __init__(
-        self,
-        device_id: str = "tv_living_room",
-        initial_state: Optional[DeviceState] = None,
-    ):
+class MockTV:
+    """Base mock representation of a physical TV hardware device."""
+
+    def __init__(self, device_id: str = "tv_living_room", initial_state: Optional[DeviceState] = None):
         self.device_id = device_id
-        self._current_state = initial_state or DeviceState(
-            power=True, volume=50.0, input_source="HDMI_1"
-        )
+        self.state = initial_state or DeviceState(power=True, volume=50.0, input_source="HDMI_1")
+
+    def get_state(self) -> DeviceState:
+        return self.state
+
+    def set_state(self, new_state: DeviceState) -> None:
+        self.state = new_state
+
+
+class MockTVAdapter:
+    """Adapter bridging PhysicalTransactionManager to MockTV hardware calls."""
+
+    def __init__(self, device_id: str = "tv_living_room", initial_state: Optional[DeviceState] = None):
+        self.device_id = device_id
+        self.tv = MockTV(device_id=device_id, initial_state=initial_state)
 
     async def observe_state(self) -> DeviceState:
         """Observes and returns current device state."""
-        return self._current_state
+        return self.tv.get_state()
 
     async def apply_state(self, target_state: DeviceState) -> Tuple[bool, Optional[str]]:
         """Applies a target state to the mock TV."""
-        self._current_state = target_state
+        self.tv.set_state(target_state)
         return True, None
 
     async def rollback(self, target_state: DeviceState) -> Tuple[bool, Optional[str]]:
         """Rolls back state to target state."""
-        self._current_state = target_state
+        self.tv.set_state(target_state)
         return True, None
