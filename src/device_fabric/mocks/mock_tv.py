ae0922ce11f343b09f1a0849e19405664fdd27e0
david cobbold <144285585+Dvcutie04@users.noreply.github.com>
2026-08-29 15:19:07 -0700

    Update mock_tv.py

--- src/device_fabric/mocks/mock_tv.py
+++ src/device_fabric/mocks/mock_tv.py
@@ -13,7 +13,6 @@ class MockTV:
         self.identity = DeviceIdentity(
             device_id=device_id,
             device_type="TV",
-            firmware_version="1.0.0",
         )
         self.capabilities = DeviceCapabilities(
             supported_operations=frozenset(["set_volume", "set_power", "set_muted", "REDUCE_VOLUME"]),
